f4c60e83d43ba61e9e1a22471308bc26d278aef8
mmishv <pinina7773@gmail.com>
2023-08-30 00:17:50 +0300

    fix typing

--- innotter/authentication/tasks.py
+++ innotter/authentication/tasks.py
@@ -5,7 +5,7 @@ from celery import shared_task
 from rest_framework import exceptions
 
 
-def perform_request(url, method, headers=None, params=None, data=None):
+def perform_request(url, method, headers=None, params=None, data=None) -> dict:
     try:
         response = requests.request(
             method, url, headers=headers, params=params, data=data
@@ -25,14 +25,14 @@ def perform_request(url, method, headers=None, params=None, data=None):
 
 
 @shared_task(queue="user_management_queue")
-def validate_token(access_token):
+def validate_token(access_token) -> dict:
     validate_jwt_url = os.getenv("VALIDATE_JWT_URL")
     headers = {"token": access_token}
     return perform_request(validate_jwt_url, "GET", headers)
 
 
 @shared_task(queue="user_management_queue")
-def change_user_block_status(access_token, user_uuid, block_status: bool):
+def change_user_block_status(access_token, user_uuid, block_status: bool) -> None:
     patch_user_url = os.getenv("PATCH_USER_URL")
     headers = {"token": access_token}
     perform_request(
@@ -44,7 +44,7 @@ def change_user_block_status(access_token, user_uuid, block_status: bool):
 
 
 @shared_task(queue="user_management_queue")
-def get_secure_data(user_id):
+def get_secure_data(user_id) -> dict:
     url = f'{os.getenv("SECURE_DATA_URL")}/{user_id}/'
     headers = {"api-key": os.getenv("API_KEY")}
     return perform_request(url, "GET", headers)
--- innotter/posts/service.py
+++ innotter/posts/service.py
@@ -23,14 +23,14 @@ class PostService:
         PostLike.objects.filter(post=post, user_uuid=user_uuid).delete()
 
     @staticmethod
-    def get_liked_posts(user_uuid):
+    def get_liked_posts(user_uuid) -> dict:
         likes = PostLike.objects.filter(user_uuid=user_uuid)
         liked_posts = [like.post for like in likes]
         post_serializer = PostSerializer(liked_posts, many=True)
         return post_serializer.data
 
     @staticmethod
-    def get_news_feed(user_uuid):
+    def get_news_feed(user_uuid) -> dict:
         one_week_ago = datetime.now() - timedelta(days=7)
         own_pages_posts = Post.objects.filter(
             Q(page__owner_uuid=user_uuid) | Q(created_at__gte=one_week_ago)
@@ -48,7 +48,7 @@ class PostService:
         return post_serializer.data
 
     @staticmethod
-    def get_page_followers_emails(page):
+    def get_page_followers_emails(page) -> list:
         followers = PageFollower.objects.filter(page=page)
         get_email_lambda = lambda user_id: get_secure_data.delay(user_id).get()["email"]
         return [get_email_lambda(follower.follower_uuid) for follower in followers]
